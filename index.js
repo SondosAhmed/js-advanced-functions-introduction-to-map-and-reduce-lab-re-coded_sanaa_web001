99201c804bb9cf62eba968919ee1bf96594f9796
Sondos Ahmed <sondosalshami@gmail.com>
2020-08-27 12:47:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+/*
+`mapToNegativize(sourceArray)`
+- `mapToNoChange(sourceArray)`
+- `mapToDouble(sourceArray)`
+- `mapToSquare(sourceArray)`*/
+sourceArray = [1,2,3];
+function mapToNegativize(sourceArray){
+ let  newArray =[];
+ for(let i=0;i<sourceArray.length;i++)
+ newArray[i]= sourceArray[i]*-1;
+return newArray;
+}
+mapToNegativize(sourceArray);
+function mapToNoChange(sourceArray){
+  return sourceArray;
+}
+mapToNegativize(sourceArray);
+function mapToDouble(sourceArray){
+  let newArray=[];
+  for(let i=0;i<sourceArray.length;i++)
+  newArray[i]= sourceArray[i]*2;
+return newArray;
+}
+function mapTosquare(sourceArray){
+  let newArray=[];
+  for(let i=0;i<sourceArray.length;i++)
+  newArray[i]=   Math.pow(sourceArray[i],2);
+  Math.pow(sourceArray[i],2)
+return newArray;
+}
